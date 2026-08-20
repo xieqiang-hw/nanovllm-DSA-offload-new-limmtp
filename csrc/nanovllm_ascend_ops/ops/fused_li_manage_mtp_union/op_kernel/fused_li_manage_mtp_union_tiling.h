00ca58e1b6f6c1668873995111ac1884e9a0fb10
xieqiang-hw <xieqiang-hw@users.noreply.github.com>
2026-08-20 17:14:25 +0800

    fix(mtp): use device-safe union tiling struct

--- csrc/nanovllm_ascend_ops/ops/fused_li_manage_mtp_union/op_kernel/fused_li_manage_mtp_union_tiling.h
+++ csrc/nanovllm_ascend_ops/ops/fused_li_manage_mtp_union/op_kernel/fused_li_manage_mtp_union_tiling.h
@@ -1,12 +1,10 @@
 #ifndef FUSED_LI_MANAGE_MTP_UNION_KERNEL_TILING_H_
 #define FUSED_LI_MANAGE_MTP_UNION_KERNEL_TILING_H_
 
-#include "register/tilingdata_base.h"
-
 namespace optiling {
-BEGIN_TILING_DATA_DEF(FusedLiManageMtpUnionTilingData)
-TILING_DATA_FIELD_DEF(uint32_t, batchSize)
-END_TILING_DATA_DEF
+struct FusedLiManageMtpUnionTilingData {
+    uint32_t batchSize;
+};
 } // namespace optiling
 
 #endif
